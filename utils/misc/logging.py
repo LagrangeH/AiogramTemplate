b99bc2f24dba31921f3ad3df337558b5b6491b79
LagrangeH <maxsergeev39@gmail.com>
2021-08-23 01:56:21 +0300

    Add loguru module instead logging module

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -1,6 +1,5 @@
-import logging
+from loguru import logger
 
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO,
-                    # level=logging.DEBUG,  # Можно заменить на другой уровень логгирования.
-                    )
+
+logger.add("info.log")
+# u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s'
